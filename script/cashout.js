685b833334a5e22ebfba77d372b006423f000e80
nahidhasan330874 <nahidhasan1005004@gmail.com>
2026-02-20 16:58:03 +0600

    created the cashout js and complete the part

--- script/cashout.js
+++ script/cashout.js
@@ -1,16 +1,52 @@
 
 document.getElementById('cashout-btn').addEventListener('click', function(){
     // 1 - get the agent number & validate
+    const cashOutNumberInput = document.getElementById('cashout-number');
+     const cashOutNumber = cashOutNumberInput.value;
 
-    // 2- get the amount , validate , convert to number 
+     if(cashOutNumber.length !=11){
+          alert(' Invalid Agent Number');
+          return;
+     }
 
-    // 3- get Current Balance , validate , convert to Number
+
+    // 2- get the amount 
+
+    const cashOutAmountInput = document.getElementById('cashout-amount');
+     const cashOutAmount = cashOutAmountInput.value;
+
+    // 3- get Current Balance 
+
+    const balanceElement = document.getElementById('balance');
+      const balance = balanceElement.innerText;
+
 
     // 4- Calculate new Balance 
 
+    const newBalance = Number(balance) - Number(cashOutAmount)
+    if(newBalance < 0) {
+        alert('Invalid Amount');
+        return;
+    }
+        console.log(newBalance);
+
     // 5- get the pin and verify
 
-    // 5-1: true :: show an alert > set balance
+    const cashOutPinInput =document.getElementById('cashout-pin');
+    const pin = cashOutPinInput.value;
 
-    // 5-2: false :: show an error alert >  retune
+    if(pin === '1234') {
+        // 5-1: true :: show an alert > set balance
+         alert('CashOut Successful');
+         balanceElement.innerText = newBalance;
+
+    }
+    else{
+        // 5-2: false :: show an error alert >  retune
+        alert('Invalid Pin');
+        return;
+    }
+            console.log(newBalance);
+ 
+
 })
