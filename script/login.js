434beffae47e2a021189f934a934521372fd8ceb
nahidhasan330874 <nahidhasan1005004@gmail.com>
2026-02-20 12:53:09 +0600

    added the js and creat the home page

--- script/login.js
+++ script/login.js
@@ -1,1 +1,26 @@
-console.log('Login the pages')
+
+
+document.getElementById('btn-login').addEventListener('click', function(){
+    // 1. get the mobile number input
+    const inputNumber = document.getElementById('input-number');
+     const  Number = inputNumber.value;
+        
+    // 2. get the pin input 
+    const pinInput = document.getElementById('input-pin');
+     const pin = pinInput.value;
+
+    // 3. match pin & mobile number 
+    if(Number == "01234567890" &&  pin == "1234" ){
+
+        // 3-1  true :: > alert> homepage
+        alert('login Success');
+
+        // window.location.replace('/home.html')
+        window.location.assign('/home.html')
+    }
+    else{
+        alert('login Failed');
+        return;
+    }
+    // 3-2  false :: > alert> return
+})
