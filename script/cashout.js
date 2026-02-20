2f2deea37ed6f6f0ffbb7484ee2fda3ef5e87725
nahidhasan330874 <nahidhasan1005004@gmail.com>
2026-02-20 15:34:16 +0600

    created the cashout section

---
+++ script/cashout.js
@@ -0,0 +1,16 @@
+
+document.getElementById('cashout-btn').addEventListener('click', function(){
+    // 1 - get the agent number & validate
+
+    // 2- get the amount , validate , convert to number 
+
+    // 3- get Current Balance , validate , convert to Number
+
+    // 4- Calculate new Balance 
+
+    // 5- get the pin and verify
+
+    // 5-1: true :: show an alert > set balance
+
+    // 5-2: false :: show an error alert >  retune
+})
